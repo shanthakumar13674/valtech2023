0395c91ec8510ed00e6dc9c8c9185284792ae66c
shanthakumar <shanthakumar@gmail.com>
2023-10-31 12:10:22 +0530

    Inital Commit

--- first-maven-project/src/main/java/com/valtech/training/first_maven_project/App.java
+++ first-maven-project/src/main/java/com/valtech/training/first_maven_project/App.java
@@ -6,6 +6,11 @@ package com.valtech.training.first_maven_project;
  */
 public class App 
 {
+	
+	public String sayHello()	{
+		return "Hello World";
+	}
+	
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
--- first-maven-project/src/test/java/com/valtech/training/first_maven_project/AppTest.java
+++ first-maven-project/src/test/java/com/valtech/training/first_maven_project/AppTest.java
@@ -24,6 +24,11 @@ public class AppTest extends TestCase {
 		return new TestSuite(AppTest.class);
 	}
 
+	public void testSayHello() {
+		App app = new App();
+		assertEquals("Hello World",app.sayHello());
+	}
+
 	/**
 	 * Rigourous Test :-)
 	 */
